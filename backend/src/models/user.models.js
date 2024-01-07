229de21e45d4058106aa856726bd7330ea0ffb0b
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-07 19:42:29 +0530

    add hooks and method for decode and encode password and geneated password token

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose,{Schema} from 'mongoose'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 
 const userSchema=new Schema({
@@ -59,7 +60,7 @@ const userSchema=new Schema({
     }
 },{timestamps:true})
 
-export const User=mongoose.model("User",userSchema)
+
 
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next()
@@ -72,3 +73,46 @@ userSchema.pre("save",async function(next){
 userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
 }
+
+userSchema.methods.generateAccessToken=function (){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    
+    )
+}
+
+userSchema.methods.generateRefreshToken=function (){
+    return jwt.sign(
+        {
+            _id:this.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generatePasswordToken=function(){
+    const resetToken=crypto.randomBytes(20).toString('hex');
+
+            this.forgotPasswordToken=crypto
+                                     .createHash('sha256')
+                                     .update(resetToken)
+                                     .digest('hex')
+
+            this.forgotPasswordExpiry=Date.now() + 15 * 60 * 1000
+
+            return resetToken;
+}
+
+
+export const User=mongoose.model("User",userSchema)
