fa0958e027df3592b6cef7f18df9f0e36e26a500
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-07 19:14:02 +0530

    add hooks and method for decode and encode password

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from 'mongoose'
+import bcrypt from 'bcrypt'
 
 
 const userSchema=new Schema({
@@ -60,3 +61,14 @@ const userSchema=new Schema({
 
 export const User=mongoose.model("User",userSchema)
 
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password=await bcrypt(this.password,10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
