874b64ff5026ab738867f2a891642246f78e5e97
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-08 19:55:09 +0530

    Add update user account management endpoints

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import {ApiResponse} from '../utlis/ApiResponse.js'
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utlis/ApiError.js";
 import jwt from "jsonwebtoken"
+import { uploadOnCloudinary } from "../utlis/cloudinary.js";
 
 const options={
     httpOnly:true,
@@ -77,11 +78,99 @@ const changePassword=asyncHandler (async (req,res)=>{
 })
 
 
+const updaterUserDetials=asyncHandler(async (req,res)=>{
+    const {fullName,email}=req.body
 
+    if(!username || !email){
+        throw new ApiError(400,"All field are required")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                fullName:fullName,
+                email:email
+            },        
+        },
+        {
+            new:true
+        }
+    ).select("-passwprd")
+
+    return res 
+              .status(200)
+              .json(
+                new ApiResponse(200,user,"User detials successfully")
+              )
+})
+
+const updateUserAvatar=asyncHandler(async (req,res)=>{
+     const avatarLocalPath=req.file?.path
+
+     if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is missing")
+     }
+
+     const avatar=await uploadOnCloudinary(avatarLocalPath)
+
+     if(!avatar){
+        throw new ApiError(400,"Error while uploading on avatar")
+     }
+
+    const user=await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+              .status(200)
+              .json(
+                new ApiResponse(200,user,"User avatar successfully")
+              )
+})
+
+const updateUserCoverImage=asyncHandler(async (req,res)=>{
+     const coverImageLocalPath=req.file?.path
+
+     if(!coverImageLocalPath){
+        throw new ApiError(400,"Cover Image file is missing")
+     }
+
+     const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+     if(!coverImage){
+        throw new ApiError(400,"Error while uploading on coverImage")
+     }
+
+    const user=await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+              .status(200)
+              .json(
+                new ApiResponse(200,user,"User avatar successfully")
+              )
+})
 
 
 export {
     getUser,
     logoutUser,
-    changePassword
+    changePassword,
+    updaterUserDetials,
+    updateUserAvatar,
+    updateUserCoverImage
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express"
-import { changePassword, getUser, logoutUser } from "../controllers/user.controllers.js"
+import { changePassword, getUser, logoutUser, updateUserAvatar, updateUserCoverImage, updaterUserDetials } from "../controllers/user.controllers.js"
 import {isUserLoggedIn} from '../middelwares/auth.middelwares.js'
+import { upload } from "../middelwares/multer.middelwares.js"
 
 const router=Router()
 
 router.route("/me").get(isUserLoggedIn,getUser)
 router.route("/logout").get(isUserLoggedIn,logoutUser)
 router.route("/change-password").get(isUserLoggedIn,changePassword)
+router.route("/update-account").get(isUserLoggedIn,updaterUserDetials)
+router.route("/avatar").get(isUserLoggedIn,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").get(isUserLoggedIn,upload.single("coverImage"),updateUserCoverImage)
 
 
 export default router;
