976cd90c0c3470745f50586779b72f602689ee55
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-08 19:06:59 +0530

    added changePassword

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -50,8 +50,38 @@ const logoutUser=asyncHandler(async (req,res)=>{
 })
 
 
+const changePassword=asyncHandler (async (req,res)=>{
+    const {oldPasssword,newPassword}=req.body
+
+    const user=await User.findById(req.user._id)
+
+    const isPasswordCorrect=await user.isPasswordCorrect(oldPasssword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password")
+     }
+
+     user.password=newPassword
+
+     await user.save({validateBeforeSave:false})
+
+     return res  
+               .status(200)
+               .json(
+                new ApiResponse(
+                    200,
+                    {},
+                    "Password Chnage Successfully"
+                )
+               )
+})
+
+
+
+
 
 export {
     getUser,
-    logoutUser
+    logoutUser,
+    changePassword
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { getUser, logoutUser } from "../controllers/user.controllers.js"
+import { changePassword, getUser, logoutUser } from "../controllers/user.controllers.js"
 import {isUserLoggedIn} from '../middelwares/auth.middelwares.js'
 
 const router=Router()
 
 router.route("/me").get(isUserLoggedIn,getUser)
 router.route("/logout").get(isUserLoggedIn,logoutUser)
+router.route("/change-password").get(isUserLoggedIn,changePassword)
 
 
 export default router;
