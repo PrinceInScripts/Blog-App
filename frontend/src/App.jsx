9c978f23dfea9b815e8b9c97b5b6a407220762b4
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-11 12:24:19 +0530

    added react hot toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react'
 import './App.css'
+import toast from 'react-hot-toast'
 
 function App() {
   
-
+  useEffect(()=>{
+   toast.success("Laod successfull");
+  },[])
   return (
     <>
       <div className='bg-black text-white text-4xl'>Welcome to here ..</div>
