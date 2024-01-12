4f3d33e72824a3ffe435164897541dd9f52062ce
Prince Kumar <princepk7878kumar@gmail.com>
2024-01-12 18:13:12 +0530

    done edit avatar and edit coverImage

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,12 +11,12 @@ app.use(cors({
 }))
 
 app.use(express.json({
-    limit:"kb"
+    limit:"16kb"
 }))
 
 app.use(express.urlencoded({
     extended:true,
-    limit:"16kb"
+    limit:"50mb"
 }))
 
 app.use(express.static("public"))
--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -84,7 +84,7 @@ const registerUser=asyncHandler (async (req,res)=>{
     const user=await User.create({
         fullName,
         avatar:avatar.url,
-        coverImage:coverImage?.url || " ",
+        coverImage:coverImage?.url || "",
         email,
         password,
         username:username.toLowerCase()
